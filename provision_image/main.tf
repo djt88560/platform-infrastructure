1bfbe1035bbaca6138f7b49e7946bc7470c4dc73
djt88560 <danj88560@gmail.com>
2026-04-20 09:32:17 +0100

    Update main.tf

--- provision_image/main.tf
+++ provision_image/main.tf
@@ -1,17 +1,115 @@
-provider "aws" {
-  region = "eu-west-2"
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
 }
 
-resource "aws_apprunner_service" "service" {
-  service_name = "template"
-  source_configuration {
-    image_repository {
-      image_identifier = var.image
-      image_repository_type = "ECR_PUBLIC"
-      image_configuration {  
-        port = "80"
+provider "aws" {
+  region = "us-east-1"
+  profile = "default"
+}
+
+# -----------------------------
+# Networking (default VPC)
+# -----------------------------
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
+}
+
+data "aws_security_group" "default" {
+  name   = "default"
+  vpc_id = data.aws_vpc.default.id
+}
+
+# -----------------------------
+# ECS Cluster
+# -----------------------------
+
+resource "aws_ecs_cluster" "this" {
+  name = "demo-cluster"
+}
+
+# -----------------------------
+# IAM Role for ECS execution
+# -----------------------------
+
+resource "aws_iam_role" "ecs_execution" {
+  name = "ecsTaskExecutionRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com"
       }
-    } 
-    auto_deployments_enabled = false
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_execution_policy" {
+  role       = aws_iam_role.ecs_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+}
+
+# -----------------------------
+# ECS Task Definition
+# -----------------------------
+
+resource "aws_ecs_task_definition" "app" {
+  depends_on = [
+    aws_iam_role_policy_attachment.ecs_execution_policy
+  ]
+
+  family                   = "demo-task"
+  requires_compatibilities = ["FARGATE"]
+  network_mode             = "awsvpc"
+  cpu                      = "256"
+  memory                   = "512"
+  execution_role_arn       = aws_iam_role.ecs_execution.arn
+
+  container_definitions = jsonencode([
+    {
+      name      = "app"
+      image     = var.image_url
+      essential = true
+
+      portMappings = [
+        {
+          containerPort = 80
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
+}
+
+# -----------------------------
+# ECS Service
+# -----------------------------
+
+resource "aws_ecs_service" "app" {
+  name            = "demo-service"
+  cluster         = aws_ecs_cluster.this.id
+  task_definition = aws_ecs_task_definition.app.arn
+  desired_count   = 1
+  launch_type     = "FARGATE"
+
+  network_configuration {
+    subnets          = data.aws_subnets.default.ids
+    security_groups  = [data.aws_security_group.default.id]
+    assign_public_ip = true
   }
 }
