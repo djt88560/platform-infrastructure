2247bda984d58a4fc3025861fe1e04462bea4b5e
Dan Jackson-Thomas <dan.jackson-thomas@npl.co.uk>
2026-05-07 16:14:25 +0100

    enabling aws logs in the task definition

--- provision_image/main.tf
+++ provision_image/main.tf
@@ -148,6 +148,7 @@ resource "aws_ecs_task_definition" "app" {
   cpu                      = "256"
   memory                   = "512"
   execution_role_arn       = aws_iam_role.ecs_execution.arn
+  
 
   container_definitions = jsonencode([
     {
@@ -161,6 +162,15 @@ resource "aws_ecs_task_definition" "app" {
           protocol      = "tcp"
         }
       ]
+      
+      logConfiguration = {
+          logDriver = "awslogs"
+          options = {
+            awslogs-group = "ecs/app"
+            awslogs-region = "us-east-1"
+            awslogs-stream-prefix = "ecs"
+          }
+        }
     }
   ])
 }
